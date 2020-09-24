aa38337ce64d77dbde4a8a02c495ae24b5b0a23c
Stefan Winter <stefanwinter@aon.at>
2020-09-24 19:08:35 +0200

    Minor changes.

---
+++ VIC/dashboard/app.py
@@ -0,0 +1,19 @@
+# This is the backend
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask-Monitoring-Dashboard tutorial'
+
+@app.route('/endpoint1')
+def endpoint1():
+    time.sleep(0.20)
+    return 'Endpoint1', 400
+
+@app.route('/endpoint2')
+def endpoint2():
+    time.sleep(5)
+    return 'Endpoint2'
